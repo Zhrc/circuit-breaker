57db02b25259cc8e6073d920e0dcf2455f89b411
Zhrc <Zhrc@users.noreply.github.com>
2021-05-04 17:08:55 +0300

    Update

--- src/API.js
+++ src/API.js
@@ -3,52 +3,82 @@ import React, {Component} from 'react';
 const CircuitBreaker = require('opossum');
 
 function asyncFunctionThatCouldFail() {
-    const fetchPromise = fetch("/public")
-        .then(response => response.json());
-       
-    return fetchPromise;
-  }
+  const fetchPromise = fetch("/public")
+      .then(response => response.json());
+      
+  return fetchPromise;
+}  
 
 const options = {
-    timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
-    errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
-    resetTimeout: 30000 // After 30 seconds, try again.
-  };
+  timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
+  errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
+  resetTimeout: 10000 // After 10 seconds, try again.
+};
 
-const breaker = new CircuitBreaker(asyncFunctionThatCouldFail, options);
-
+  
 class API extends Component {
-    state = {
-        time: "",        
-      }
-    componentDidMount() {
-        try {
-          setInterval(async () => {
-            // const res = await fetch("/public");
-            // const response = await res.json();
 
-            // this.setState({
-            //   time: response.message,              
-            // })
-            breaker.fire()
-                .then((response) => {
-                    this.setState({
-                        time: response.message,              
-                    })
+  state = {
+      circuitBreakerState: "",      
+      message: "",        
+  };
+  constructor() {
+    super();
+    this.breaker = new CircuitBreaker(asyncFunctionThatCouldFail, options);
+    
+    this.breaker.on("open",
+    () => {
+      this.setState({
+        circuitBreakerState: "open",
+      })
+    });
+    this.breaker.on("halfOpen",
+      () => {
+        this.setState({
+          circuitBreakerState: "halfOpen",
+      })
+    });
+    this.breaker.on("close",
+      () => {
+        this.setState({
+          circuitBreakerState: "closed",
+      })
+    });    
+  };
+  
+  
+  componentDidMount() {
+    try {
+      setInterval(async () => {
+        this.breaker.fire()
+            .then((response) => {
+                this.setState({
+                    message: `Current server time is: ${response.message}`,              
                 })
-          }, 1000);
-        } catch(e) {
-          console.log(e);
-        }
+            })
+            .catch(() => {
+              var currentDate = new Date(); 
+              var currentTime = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+              this.setState({
+                message: `Current server time is: ${currentTime}`,              
+            })
+              }              
+            )
+      }, 1000);
+    } catch(e) {
+      console.log(e);
+    }
   }
 
-    render () {
-        return (
-            <div>
-                <h1>API</h1>
-                Current time is: {this.state.time}
-            </div>
-        )
+  render () {
+    return (
+        <div>
+            <h1>API</h1>
+            <p>circuitBreakerState:{this.state.circuitBreakerState}</p>
+            <p>{this.state.message}</p>
+            <p>{JSON.stringify(this.breaker.stats)}</p>
+        </div>
+      )
     }
 }
 
